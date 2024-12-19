1143784fe233d8aa86a037a51508d33f5abe924d
“ryohei-ebisawa” <xebisawa.ryohei@contract.isid.co.jp>
2024-12-19 09:11:35 +0900

    feat: Enhance AWS integration by adding environment bindings for DynamoDB

--- src/di/LambdaPortsOutImpl.ts
+++ src/di/LambdaPortsOutImpl.ts
@@ -1,11 +1,12 @@
 import { Context } from 'hono';
-import { Env } from '../env';
 import { LoadPresentationId, StorePresentationId } from '../ports/out/session';
 import { PortsOut } from './PortsOut';
 import { PresentationIdDynamo } from '../adapters/out/session/PresentationIdDynamo';
 import { DynamoDB } from 'oid4vc-core/dynamodb';
 import { DynamoDBClient, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { env } from 'hono/adapter';
+import { Bindings } from '../env';
 
 export const createDynamoDBClient = (config?: DynamoDBClientConfig) => {
   const client = new DynamoDBClient({
@@ -19,14 +20,16 @@ export const createDynamoDBClient = (config?: DynamoDBClientConfig) => {
 export class LambdaPortsOutImpl implements PortsOut {
   #presentationIdDynamo: PresentationIdDynamo;
 
-  constructor(ctx: Context<Env>) {
+  constructor(ctx: Context) {
+    const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, TABLE_NAME } =
+      env<Bindings>(ctx);
     const client = createDynamoDBClient({
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY,
       },
     });
-    const dynamo = new DynamoDB(client, process.env.TABLE_NAME ?? '');
+    const dynamo = new DynamoDB(client, TABLE_NAME);
     this.#presentationIdDynamo = new PresentationIdDynamo(dynamo);
   }
 
--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,7 @@
 export interface Bindings {
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
+  TABLE_NAME: string;
   API_BASE_URL: string;
   INIT_TRANSACTION_PATH: string;
   WALLET_RESPONSE_PATH: string;
