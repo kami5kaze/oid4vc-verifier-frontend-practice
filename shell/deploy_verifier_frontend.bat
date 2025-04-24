194f3a1be369346205920a6052d709c4a63d255a
ryohei-ebisawa <xebisawa.ryohei@contract.isid.co.jp>
2025-04-24 12:15:22 +0900

    fix: setup script

--- shell/deploy_verifier_frontend.bat
+++ shell/deploy_verifier_frontend.bat
@@ -8,7 +8,8 @@ echo 続行するにはEnterキーを押してください。
 pause > nul
 
 :: 各モジュールのディレクトリを設定
-set "BUILD_DIR=build"
+set "CURRENT_DIR=%CD%"
+set "BUILD_DIR=%CURRENT_DIR%\build"
 set "CORE_DIR=%BUILD_DIR%\oid4vc-core"
 set "PREX_DIR=%BUILD_DIR%\oid4vc-prex"
 set "CBOR_DIR=%BUILD_DIR%\mdoc-cbor-ts"
